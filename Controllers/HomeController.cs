9f881e3d11d8aca552906fe3c1848c21a7cb68f7
Quaad <ajwood1998@hotmail.co.uk>
2025-08-16 21:54:58 +0200

    created internal database using the entity framework. Used the Razor template engine to create a dynamic page to render out all the expenses and added the functionality to also edit and delete expenses.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,9 +8,12 @@ namespace SpendSmart.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly SpendSmartDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, SpendSmartDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -25,16 +28,49 @@ namespace SpendSmart.Controllers
 
         public IActionResult Expenses()
         {
-            return View();
+            var allExpenses = _context.Expenses.ToList();
+
+            var totalExpenses = allExpenses.Sum(x => x.Value);
+
+            ViewBag.Expenses = totalExpenses;
+
+            return View(allExpenses);
         }
 
-        public IActionResult CreateEditExpense()
+        public IActionResult CreateEditExpense(int? id)
         {
+            //editing -> load an expense by id
+            if(id != null)
+            {
+                var ExpenseInDb = _context.Expenses.FirstOrDefault(expense => expense.Id == id);
+                return View(ExpenseInDb);
+            }
             return View();
         }
 
         public IActionResult CreateEditExpenseForm(Expense model)
         {
+            if(model.Id == 0)
+            {
+                //create
+                _context.Expenses.Add(model);
+
+            } else
+            {
+                //editing
+                _context.Expenses.Update(model);
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Expenses");
+        }
+
+        public IActionResult DeleteExpense(int id) 
+        {
+            var ExpenseInDb = _context.Expenses.FirstOrDefault(expense => expense.Id == id);
+            _context.Expenses.Remove(ExpenseInDb);
+            _context.SaveChanges();
             return RedirectToAction("Expenses");
         }
 
